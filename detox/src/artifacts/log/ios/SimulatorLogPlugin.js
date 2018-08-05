6376b6c1aa47485019e3c8546c757811e4dddf98
Yaroslav Serhieiev <yaroslavs@wix.com>
2018-08-05 13:49:49 +0300

    test: adding SimulatorLogPlugin integration test suite

--- detox/src/artifacts/log/ios/SimulatorLogPlugin.js
+++ detox/src/artifacts/log/ios/SimulatorLogPlugin.js
@@ -1,47 +1,102 @@
+const _ = require('lodash');
+const fs = require('fs-extra');
+const stream = require('stream');
 const tempfile = require('tempfile');
+const log = require('../../../utils/logger').child({ __filename });
+const Artifact = require('../../templates/artifact/Artifact');
 const LogArtifactPlugin = require('../LogArtifactPlugin');
-const SimulatorLogRecording = require('./SimulatorLogRecording');
+const interruptProcess = require('../../../utils/interruptProcess');
+const { spawnAndLog } = require('../../../utils/exec');
 
 class SimulatorLogPlugin extends LogArtifactPlugin {
   constructor(config) {
     super(config);
 
     this.appleSimUtils = config.appleSimUtils;
+    this.stdoutTailProcess = null;
+    this.stderrTailProcess = null;
+    this.parentStream = new stream.PassThrough();
   }
 
-  async onShutdownDevice(event) {
-    await super.onShutdownDevice(event);
-
-    if (this.currentRecording) {
-      await this.currentRecording.stop();
-    }
+  async onBeforeLaunchApp(event) {
+    await super.onBeforeLaunchApp(event);
+    await this._tryTerminateTails();
   }
 
   async onLaunchApp(event) {
     await super.onLaunchApp(event);
+    await this._spawnTails(event.deviceId);
+  }
 
-    if (this.currentRecording) {
-      await this.currentRecording.start();
+  async onShutdownDevice(event) {
+    await this._tryTerminateTails();
+  }
+
+  async onAfterAll() {
+    await this._tryTerminateTails();
+  }
+
+  async onTerminate() {
+    await this._tryTerminateTails();
+  }
+
+  async _spawnTails(deviceId) {
+    const { stdout, stderr } = this.appleSimUtils.getLogsPaths(deviceId);
+
+    this.stdoutTailProcess = spawnAndLog('tail', [stdout], { silent: true });
+    this.stderrTailProcess = spawnAndLog('tail', [stderr], { silent: true });
+
+    this.stdoutTailProcess.childProcess.stdout.pipe(this.parentStream, { end: false });
+    this.stderrTailProcess.childProcess.stdout.pipe(this.parentStream, { end: false });
+  }
+
+  async _tryTerminateTails() {
+    if (this.stdoutTailProcess) {
+      await interruptProcess(this.stdoutTailProcess);
+      this.stdoutTailProcess = null;
+    }
+
+    if (this.stderrTailProcess) {
+      await interruptProcess(this.stderrTailProcess);
+      this.stderrTailProcess = null;
     }
   }
 
   createStartupRecording() {
-    return this._createRecording(true);
+    return this.createTestRecording();
   }
 
   createTestRecording() {
-    return this._createRecording(false);
-  }
+    const parentStream = this.parentStream;
+    const logPath = tempfile('.log');
+    let logStream = null;
 
-  _createRecording(readFromBeginning) {
-    const udid = this.context.deviceId;
-    const { stdout, stderr } = this.appleSimUtils.getLogsPaths(udid);
+    return new Artifact({
+      name: 'SimulatorLogRecording',
 
-    return new SimulatorLogRecording({
-      temporaryLogPath: tempfile('.log'),
-      logStderr: stderr,
-      logStdout: stdout,
-      readFromBeginning,
+      async start() {
+        log.trace({ event: 'CREATE_STREAM '}, `creating append-only stream to: ${logPath}`);
+        logStream = fs.createWriteStream(logPath, { flags: 'a' });
+        parentStream.pipe(logStream);
+      },
+
+      async stop() {
+        parentStream.unpipe(logStream);
+        logStream.end();
+      },
+
+      async save(artifactPath) {
+        if (await fs.exists(logPath)) {
+          log.debug({ event: 'MOVE_FILE' }, `moving "${logPath}" to ${artifactPath}`);
+          await fs.move(logPath, artifactPath);
+        } else {
+          log.error({ event: 'MOVE_FILE_ERROR'} , `did not find temporary log file: ${logPath}`);
+        }
+      },
+
+      async discard() {
+        await fs.remove(logPath);
+      }
     });
   }
 }
---
+++ detox/src/artifacts/log/ios/SimulatorLogPlugin.test.js
@@ -0,0 +1,102 @@
+jest.mock('../../../utils/argparse');
+
+const tempfile = require('tempfile');
+const fs = require('fs-extra');
+const path = require('path');
+
+describe('SimulatorLogPlugin', () => {
+  let argparse;
+  let fakePathBuilder;
+  let fakeAppleSimUtils;
+  let artifactsManager;
+  let SimulatorLogPlugin;
+  let ArtifactsManager;
+  let createdArtifacts;
+
+  beforeEach(() => {
+    argparse = require('../../../utils/argparse');
+    argparse.getArgValue.mockImplementation((key) => {
+      switch (key) {
+        case 'record-logs': return 'all';
+        case 'loglevel': return 'trace';
+        case 'artifacts-location': return path.dirname(tempfile(''));
+        default: throw new Error(`unexpected argparse.getArgValue mock call: ${key}`);
+      }
+    });
+
+    SimulatorLogPlugin = require('./SimulatorLogPlugin');
+    ArtifactsManager = require('../../ArtifactsManager');
+
+    createdArtifacts = [];
+    fakePathBuilder = {
+      buildPathForTestArtifact: jest.fn((_, summary) => {
+        const artifactPath = tempfile(summary ? '.startup.log' : '.log');
+        createdArtifacts.push(artifactPath);
+
+        return artifactPath;
+      }),
+    };
+
+    fakeAppleSimUtils = {
+      logs: Object.freeze({
+        stdout: tempfile('.stdout.log'),
+        stderr: tempfile('.stderr.log'),
+      }),
+
+      getLogsPaths() {
+        return this.logs;
+      }
+    };
+
+    artifactsManager = new ArtifactsManager(fakePathBuilder);
+    artifactsManager.registerArtifactPlugins({
+      log: (api) => new SimulatorLogPlugin({
+        api,
+        appleSimUtils: fakeAppleSimUtils,
+      }),
+    })
+  });
+
+  it('should work through-out boots, launches and relaunches', async () => {
+    debugger;
+    await artifactsManager.onBootDevice({ deviceId: 'booted' });
+    await fs.appendFile(fakeAppleSimUtils.logs.stdout, 'boot line\n');
+    await fs.appendFile(fakeAppleSimUtils.logs.stderr, 'boot line\n');
+
+    await artifactsManager.onBeforeAll();
+    await artifactsManager.onBeforeLaunchApp({ device: 'booted', bundleId: 'com.test' });
+    await fs.remove(fakeAppleSimUtils.logs.stdout);
+    await fs.remove(fakeAppleSimUtils.logs.stderr);
+    await artifactsManager.onLaunchApp({ device: 'booted', bundleId: 'com.test', pid: 8000 });
+    await fs.appendFile(fakeAppleSimUtils.logs.stdout, 'launch line\n');
+    await fs.appendFile(fakeAppleSimUtils.logs.stderr, 'launch line\n');
+
+    await artifactsManager.onBeforeEach({ title: 'test', fullName: 'some test', status: 'running'});
+    await fs.appendFile(fakeAppleSimUtils.logs.stdout, 'in-test line\n');
+    await fs.appendFile(fakeAppleSimUtils.logs.stderr, 'in-test line\n');
+
+    await artifactsManager.onBeforeLaunchApp({ device: 'booted', bundleId: 'com.test' });
+    await fs.remove(fakeAppleSimUtils.logs.stdout);
+    await fs.remove(fakeAppleSimUtils.logs.stderr);
+    await artifactsManager.onLaunchApp({ device: 'booted', bundleId: 'com.test', pid: 8001 });
+
+    await fs.appendFile(fakeAppleSimUtils.logs.stdout, 'post-relaunch line\n');
+    await fs.appendFile(fakeAppleSimUtils.logs.stderr, 'post-relaunch line\n');
+
+    await artifactsManager.onAfterEach({ title: 'test', fullName: 'some test', status: 'passed'});
+    await artifactsManager.onAfterAll();
+
+    expect(fakePathBuilder.buildPathForTestArtifact).toHaveBeenCalledTimes(2);
+    for (const artifact of createdArtifacts) {
+      const contents = await fs.readFile(artifact, 'utf8');
+      expect(sanitizeGuids(contents)).toMatchSnapshot();
+    }
+  });
+
+  function sanitizeGuids(str) {
+    var guid = /([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/ig;
+    var blank = '00000000-0000-0000-0000-000000000000';
+
+    return str.replace(guid, blank);
+  }
+});
